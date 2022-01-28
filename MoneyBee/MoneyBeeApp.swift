ab0bebd0f0c4159807629f3bfee15cd18ed1e92d
Eric Cheung <eric1162004@hotmail.com>
2022-01-28 09:34:06 -0800

    add project init files

---
+++ MoneyBee/MoneyBeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyBeeApp.swift
+//  MoneyBee
+//
+//  Created by Eric Cheung on 2022-01-28.
+//
+
+import SwiftUI
+
+@main
+struct MoneyBeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
